4e6363accf0479e430f67411690e822a45be4fdd
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-12-29 23:30:56 +0100

    Scanner worker implementation

--- engordio/scanner_worker.py
+++ engordio/scanner_worker.py
@@ -1,2 +1,9 @@
-def scanner_worker(scan_queue, decompress_queue, scan_fn):
-    list(scan_fn(scan_queue.get()))
+from engordio.signals import Scan, Decompress
+
+
+def scanner_worker(filepath, scan_queue, decompress_queue, scan_fn):
+    for e in scan_fn(filepath):
+        if isinstance(e, Scan):
+            scan_queue.put(e.path)
+        else:
+            decompress_queue.put(e.path)
--- tests/unit/test_scanner_worker.py
+++ tests/unit/test_scanner_worker.py
@@ -1,6 +1,7 @@
 import queue
 
 from engordio.scanner_worker import scanner_worker
+from engordio.signals import Scan, Decompress
 
 
 def test_call_scanner_with_file():
@@ -11,10 +12,33 @@ def test_call_scanner_with_file():
     def scanner_scan(filepath):
         nonlocal called_with
         called_with = filepath
-        yield
+        if False:
+            yield
 
-    scan_queue.put('FOO')
-    scanner_worker(scan_queue, decompress_queue, scanner_scan)
+    scanner_worker('FOO', scan_queue, decompress_queue, scanner_scan)
 
     assert called_with == 'FOO'
 
+
+def test_add_to_scan_queue_scan_signals_from_scanner():
+    scan_queue = queue.Queue()
+    decompress_queue = queue.Queue()
+
+    def scanner_scan(filepath):
+        yield Scan(filepath)
+
+    scanner_worker('FOO', scan_queue, decompress_queue, scanner_scan)
+
+    assert scan_queue.get_nowait() == 'FOO'
+
+
+def test_add_to_decompress_queue_decompress_signals_from_scanner():
+    scan_queue = queue.Queue()
+    decompress_queue = queue.Queue()
+
+    def scanner_scan(filepath):
+        yield Decompress(filepath)
+
+    scanner_worker('FOO', scan_queue, decompress_queue, scanner_scan)
+
+    assert decompress_queue.get_nowait() == 'FOO'
