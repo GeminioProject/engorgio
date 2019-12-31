45382211b1a0dc4ef88940219d243ce55e3a5c49
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-12-31 21:05:57 +0100

    Document entities.

---
+++ engordio/__init__.py
@@ -0,0 +1,24 @@
+"""
+Engordio
+========
+
+Architecture
+------------
+
+Engordio is made of a list of entities that run concurrently and exchange
+messages among them.
+
+Those entities are started and configured by the module `master`.
+
+Entities
+~~~~~~~~
+
+* `scanner`: Find files and directories to be expanded.
+* `decompressor`: Expands archives and compressed files.
+* `replacer`: Move decompressed files to their final destination.
+* `feedbacker`: Provides feedback for the user.
+* `metadater`: Generate a file with the metadata lost from the decompression.
+* `logger`: Generate a log stream for the user.
+* `stopper`: Signal other entities to finish when there is nothing else to do.
+
+"""
