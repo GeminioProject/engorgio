20904ed2413595636f71136411e8ff09b09661f1
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-12-31 19:22:37 +0100

    Scan split in two functions, one for unknown paths and other for scanning directories.
    Co-authored-by: Leticia Garcia <leti.garcia.martin@gmail.com>

--- engordio/scanner.py
+++ engordio/scanner.py
@@ -3,29 +3,34 @@ import os
 from engordio.signals import FileFound, DirFound, SymlinkFound, SpecialFileFound
 
 
-def scan(path):
+def classify_path(path):
+    """
+    Generate a signal based on the nature of the given path.
+
+    """
+    if os.path.islink(path):
+        return SymlinkFound(path=path)
+    elif os.path.isfile(path):
+        return FileFound(path=path)
+    elif os.path.isdir(path):
+        return DirFound(path=path)
+    else:
+        return SpecialFileFound(path=path)
+
+
+def scandir(path):
     """
     Generate a signal for every file and directory found in path.
 
-    On files it generates a `FileFound` signal and `DirFound` on directories.
-
     """
-    if os.path.isfile(path):
-        if os.path.islink(path):
-            yield SymlinkFound(path=path)
-        else:
-            yield FileFound(path=path)
-    elif os.path.isdir(path):
-        with os.scandir(path) as it:
-            for entry in it:
-                if entry.is_file():
-                    if entry.is_symlink():
-                        yield SymlinkFound(path=entry.path)
-                    else:
-                        yield FileFound(path=entry.path)
-                elif entry.is_dir():
-                    yield DirFound(path=entry.path)
-                else:
-                    yield SpecialFileFound(path=entry.path)
-    else:
-        yield SpecialFileFound(path=path)
+    with os.scandir(path) as dircontent:
+        for entry in dircontent:
+            if entry.is_symlink():
+                yield SymlinkFound(path=entry.path)
+            elif entry.is_file():
+                yield FileFound(path=entry.path)
+            elif entry.is_dir():
+                yield DirFound(path=entry.path)
+            else:
+                yield SpecialFileFound(path=entry.path)
+
--- tests/unit/test_scanner.py
+++ tests/unit/test_scanner.py
@@ -3,16 +3,17 @@ import os
 import tempfile
 
 
-from engordio.scanner import scan
+from engordio.scanner import classify_path, scandir
 from engordio.signals import FileFound, DirFound, SymlinkFound, SpecialFileFound
 
 
-def test_return_a_filefound_if_regular_file():
+
+def test_classify_path_returns_a_filefound_if_regular_file():
     with tempfile.NamedTemporaryFile() as file:
-        assert list(scan(file.name)) == [FileFound(path=file.name)]
+        assert classify_path(file.name) == FileFound(path=file.name)
 
 
-def test_return_a_symlinkfound_if_symlink_to_regular_file():
+def test_classify_path_returns_a_symlinkfound_if_symlink_to_regular_file():
     with tempfile.TemporaryDirectory() as path:
         filepath = os.path.join(path, 'regular_file')
         symlink_path = filepath + '_symlink'
@@ -20,36 +21,35 @@ def test_return_a_symlinkfound_if_symlink_to_regular_file():
         open(filepath, 'w').close()  # Equivalent to touch command
         os.symlink(filepath, symlink_path)
 
-        assert list(scan(symlink_path)) == [SymlinkFound(path=symlink_path)]
+        assert classify_path(symlink_path) == SymlinkFound(path=symlink_path)
 
 
-def test_return_a_specialfilefound_if_non_regular_file():
+def test_classify_path_returns_a_specialfilefound_if_non_regular_file():
     with tempfile.TemporaryDirectory() as path:
         pipe_path = os.path.join(path, 'pipe')
         os.mkfifo(pipe_path)
 
-        assert list(scan(pipe_path)) == [SpecialFileFound(path=pipe_path)]
+        assert classify_path(pipe_path) == SpecialFileFound(path=pipe_path)
+
+
+def test_classify_path_returns_a_dirfound_if_dir():
+    with tempfile.TemporaryDirectory() as path:
+        assert classify_path(path) == DirFound(path=path)
 
 
 # TODO: test for other non regular files
 
 
-def test_scan_is_a_generator():
-    assert inspect.isgeneratorfunction(scan)
+def test_scandir_is_a_generator():
+    assert inspect.isgeneratorfunction(scandir)
 
 
-def test_return_an_empty_list_if_dir_empty():
+def test_scandir_returns_an_empty_list_if_dir_empty():
     with tempfile.TemporaryDirectory() as path:
-        assert list(scan(path)) == []
+        assert list(scandir(path)) == []
 
 
-def test_return_a_list_of_signals_if_dir():
-    with tempfile.TemporaryDirectory() as path:
-        with tempfile.NamedTemporaryFile(dir=path) as file, tempfile.TemporaryDirectory(dir=path) as subdir:
-            assert set(scan(path)) == {FileFound(path=file.name), DirFound(path=subdir)}
-
-
-def test_return_a_symlinkfound_if_dir_contains_symlink_to_regular_file():
+def test_scandir_return_a_symlinkfound_if_dir_contains_symlink_to_regular_file():
     with tempfile.TemporaryDirectory() as path:
         filepath = os.path.join(path, 'regular_file')
         symlink_path = filepath + '_symlink'
@@ -57,18 +57,32 @@ def test_return_a_symlinkfound_if_dir_contains_symlink_to_regular_file():
         open(filepath, 'w').close()  # Equivalent to touch command
         os.symlink(filepath, symlink_path)
 
-        assert set(scan(path)) == {FileFound(path=filepath), SymlinkFound(path=symlink_path)}
+        assert SymlinkFound(path=symlink_path) in set(scandir(path))
 
 
-def test_return_a_specialfilefound_if_dir_contains_a_non_regular_file():
+def test_scandir_return_a_specialfilefound_if_dir_contains_a_non_regular_file():
     with tempfile.TemporaryDirectory() as path:
         pipe_path = os.path.join(path, 'pipe')
         os.mkfifo(pipe_path)
 
-        assert list(scan(path)) == [SpecialFileFound(path=pipe_path)]
+        assert list(scandir(path)) == [SpecialFileFound(path=pipe_path)]
 
 
-def test_return_a_list_of_signals_if_dir_contains_dotfile():
+def test_scandir_return_a_filefound_if_dir_contains_a_regular_file():
+    with tempfile.TemporaryDirectory() as path:
+        filepath = os.path.join(path, 'regular_file')
+        open(filepath, 'w').close()  # Equivalent to touch command
+
+        assert list(scandir(path)) == [FileFound(path=filepath)]
+
+
+def test_scandir_returns_a_dirfound_if_dir_contains_dir():
+    with tempfile.TemporaryDirectory() as path1:
+        with tempfile.TemporaryDirectory(dir=path1) as path2:
+            assert list(scandir(path1)) == [DirFound(path=path2)]
+
+
+def test_scandir_return_a_list_of_signals_if_dir_contains_dotfile():
     with tempfile.TemporaryDirectory() as path:
         with tempfile.NamedTemporaryFile(prefix='.', dir=path) as file, tempfile.TemporaryDirectory(dir=path) as subdir:
-            assert set(scan(path)) == {FileFound(path=file.name), DirFound(path=subdir)}
+            assert set(scandir(path)) == {FileFound(path=file.name), DirFound(path=subdir)}
